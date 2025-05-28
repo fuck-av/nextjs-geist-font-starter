2b68355c68c018c6e62d61ac6b0c29892ba17563
翻墙.COM <god.king.1995.11.17@gmail.com>
2025-05-29 00:10:23 +0800

    Add .next/static/chunks/src_app_page_tsx_c0237562._.js 1748448622226

---
+++ .next/static/chunks/src_app_page_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_becb1c9b._.js",
+  "static/chunks/src_850751a6._.js",
+  "static/chunks/src_app_styles_3c335735.css"
+],
+    source: "dynamic"
+});
